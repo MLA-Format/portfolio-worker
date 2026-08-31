920d675140fefd18ba0dbda06ee673a7f1570d10
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-08-30 20:49:15 -0400

    Added workers default entry point.

--- src/entry.py
+++ src/entry.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from workers import asgi
 
 app = FastAPI()
 
 @app.get("/")
-async def fetch(self, request):
-        return {"Hello": "World"}
+async def root():
+    return {"Hello": "World"}
+
+Default = asgi.entrypoint(app)
