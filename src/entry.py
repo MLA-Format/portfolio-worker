1c32bfc457eb8486199f3579c0ee64737f37314b
MLA-Format <122707566+MLA-Format@users.noreply.github.com>
2026-09-02 07:05:15 -0400

    Added todo; removed live SQL query api.

--- src/entry.py
+++ src/entry.py
@@ -3,18 +3,22 @@ from workers import asgi
 
 app = FastAPI()
 
+# @app.get("/")
+# async def root():
+#     return {"H12ello": "World"}
+
+# @app.get("/sql_test")
+# async def sql_test(req: Request):
+#     env = req.scope["env"]
+#     try:
+#         sql_stmt = await env.PORTFOLIO_DB_BINDING.prepare("SELECT * FROM skills").run()
+#         return sql_stmt.get("results", None)
+#     except Exception as e:
+#         return {"message": "Database query failed",
+#                 "error": str(e)}
+
 @app.get("/")
 async def root():
-    return {"H12ello": "World"}
-
-@app.get("/sql_test")
-async def sql_test(req: Request):
-    env = req.scope["env"]
-    try:
-        sql_stmt = await env.PORTFOLIO_DB_BINDING.prepare("SELECT * FROM skills").run()
-        return sql_stmt.get("results", None)
-    except Exception as e:
-        return {"message": "Database query failed",
-                "error": str(e)}
+    return "API in development."
 
 Default = asgi.entrypoint(app)
